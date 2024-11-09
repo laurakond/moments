b1277139149fb69f9b39a03cac7b02b5c2d2e04d
Laura Kondrataite <laura.kondrataite@gmail.com>
2024-11-09 08:02:10 +0000

    add burger toggle

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,19 +9,22 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
 
-        const handleSignOut = async () => {
+    const {expanded, setExpanded, ref} = useClickOutsideToggle();
+
+    const handleSignOut = async () => {
         try {
-            await axios.post("dj-rest-auth/logout/");
-            setCurrentUser(null);
+        await axios.post("dj-rest-auth/logout/");
+        setCurrentUser(null);
         } catch (err) {
-            console.log(err);
+        console.log(err);
         }
-        };
+    };
 
     const addPostIcon = (
         <NavLink
@@ -79,7 +82,12 @@ const NavBar = () => {
     );
 
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar
+        expanded={expanded}
+        className={styles.NavBar}
+        expand="md"
+        fixed="top"
+        >
         <Container>
             <NavLink to="/">
             <Navbar.Brand>
@@ -87,14 +95,18 @@ const NavBar = () => {
             </Navbar.Brand>
             </NavLink>
             {currentUser && addPostIcon}
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Toggle
+                ref={ref}
+                onClick={() => setExpanded(!expanded)}
+                aria-controls="basic-navbar-nav"
+            />
             <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto text-left">
                 <NavLink
-                exact
-                className={styles.NavLink}
-                activeClassName={styles.Active}
-                to="/"
+                    exact
+                    className={styles.NavLink}
+                    activeClassName={styles.Active}
+                    to="/"
                 >
                 <i className="fas fa-home"></i>Home
                 </NavLink>
